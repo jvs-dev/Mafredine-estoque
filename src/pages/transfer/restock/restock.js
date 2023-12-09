d294b418a1882ec7821054d732ecb3059bea3137
joão vitor <shepherdcom12@gmail.com>
2023-12-09 10:30:59 -0300

    update database

--- src/pages/transfer/restock/restock.js
+++ src/pages/transfer/restock/restock.js
@@ -1,15 +1,15 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 const firebaseConfig = {
-    apiKey: "AIzaSyDiTWkrXRNH4XlHNHIh8RlMKMoArVULYyE",
-    authDomain: "marktec-deposit.firebaseapp.com",
-    projectId: "marktec-deposit",
-    storageBucket: "marktec-deposit.appspot.com",
-    messagingSenderId: "158740682122",
-    appId: "1:158740682122:web:c80c33a77fad7e20b22473"
+    apiKey: "AIzaSyAjMEAVlO2ms0ZL2HxFTXMplu8ikz3mQx8",
+    authDomain: "mafredine-estoque.firebaseapp.com",
+    projectId: "mafredine-estoque",
+    storageBucket: "mafredine-estoque.appspot.com",
+    messagingSenderId: "552286796233",
+    appId: "1:552286796233:web:05ca7d9fce4ea834be1018"
 };
 const app = initializeApp(firebaseConfig);
-import { getAuth, createUserWithEmailAndPassword, sendEmailVerification, onAuthStateChanged, sendPasswordResetEmail, signOut } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-auth.js";
-import { getFirestore, collection, addDoc, doc, setDoc, onSnapshot, query, where, updateDoc, arrayUnion, arrayRemove, getDoc, getDocs, serverTimestamp, increment } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-firestore.js";
+import { getAuth, createUserWithEmailAndPassword, sendEmailVerification, onAuthStateChanged, sendPasswordResetEmail, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore, collection, addDoc, doc, setDoc, onSnapshot, query, where, updateDoc, arrayUnion, arrayRemove, getDoc, getDocs, serverTimestamp, increment } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 const db = getFirestore(app);
 const auth = getAuth();
 let closeSectionSelectOutput = document.getElementById("closeSectionSelectRestock")
